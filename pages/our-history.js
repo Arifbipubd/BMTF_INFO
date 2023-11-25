3989d6a62a98357e49112b9ee98c8f17678a9c95
Arifbipubd <arifulislambipubd@gmail.com>
2023-11-25 15:39:43 +0600

    our history page design

--- pages/our-history.js
+++ pages/our-history.js
@@ -40,112 +40,132 @@ export default function ServiceSingle1() {
         <div className='industify_fn_sidebarpage'>
           <div className='container'>
             {/*History Timeline Start */}
-            <div className='container'>
-              <div className='ourHistory'>
-                <section id='timeline'>
-                  <h1>A Flexbox Timeline</h1>
-                  <p class='leader'>
-                    All cards must be the same height and width for space
-                    calculations on large screens.
-                  </p>
-                  <div class='demo-card-wrapper'>
-                    <div class='demo-card demo-card--step1'>
-                      <div class='head'>
-                        <div class='number-box'>
-                          <span>01</span>
-                        </div>
-                        <h2>
-                          <span class='small'>Subtitle</span> Technology
-                        </h2>
+            <div className='ourHistory'>
+              <section id='timeline'>
+                <div class='demo-card-wrapper'>
+                  <div class='demo-card demo-card--step1'>
+                    <div class='head'>
+                      <div class='number-box'>
+                        <span>01</span>
                       </div>
-                      <div class='body'>
-                        <p>
-                          Lorem ipsum dolor sit amet, consectetur adipisicing
-                          elit. Soluta reiciendis deserunt doloribus
-                          consequatur, laudantium odio dolorum laboriosam.
-                        </p>
-                        <img src='http://placehold.it/1000x500' alt='Graphic' />
-                      </div>
+                      <h2>
+                        <span class='small'>In</span> 1963
+                      </h2>
                     </div>
-
-                    <div class='demo-card demo-card--step2'>
-                      <div class='head'>
-                        <div class='number-box'>
-                          <span>02</span>
-                        </div>
-                        <h2>
-                          <span class='small'>Subtitle</span> Confidence
-                        </h2>
-                      </div>
-                      <div class='body'>
-                        <p>
-                          Lorem ipsum dolor sit amet, consectetur adipisicing
-                          elit. Soluta reiciendis deserunt doloribus
-                          consequatur, laudantium odio dolorum laboriosam.
-                        </p>
-                        <img src='http://placehold.it/1000x500' alt='Graphic' />
-                      </div>
-                    </div>
-
-                    <div class='demo-card demo-card--step3'>
-                      <div class='head'>
-                        <div class='number-box'>
-                          <span>03</span>
-                        </div>
-                        <h2>
-                          <span class='small'>Subtitle</span> Adaptation
-                        </h2>
-                      </div>
-                      <div class='body'>
-                        <p>
-                          Lorem ipsum dolor sit amet, consectetur adipisicing
-                          elit. Soluta reiciendis deserunt doloribus
-                          consequatur, laudantium odio dolorum laboriosam.
-                        </p>
-                        <img src='http://placehold.it/1000x500' alt='Graphic' />
-                      </div>
-                    </div>
-
-                    <div class='demo-card demo-card--step4'>
-                      <div class='head'>
-                        <div class='number-box'>
-                          <span>04</span>
-                        </div>
-                        <h2>
-                          <span class='small'>Subtitle</span> Consistency
-                        </h2>
-                      </div>
-                      <div class='body'>
-                        <p>
-                          Lorem ipsum dolor sit amet, consectetur adipisicing
-                          elit. Soluta reiciendis deserunt doloribus
-                          consequatur, laudantium odio dolorum laboriosam.
-                        </p>
-                        <img src='http://placehold.it/1000x500' alt='Graphic' />
-                      </div>
-                    </div>
-
-                    <div class='demo-card demo-card--step5'>
-                      <div class='head'>
-                        <div class='number-box'>
-                          <span>05</span>
-                        </div>
-                        <h2>
-                          <span class='small'>Subtitle</span> Conversion
-                        </h2>
-                      </div>
-                      <div class='body'>
-                        <p>
-                          Lorem ipsum dolor sit amet, consectetur adipisicing
-                          elit. Soluta reiciendis deserunt doloribus
-                          consequatur, laudantium odio dolorum laboriosam.
-                        </p>
-                        <img src='http://placehold.it/1000x500' alt='Graphic' />
-                      </div>
+                    <div class='body'>
+                      <p>
+                        The Bangladesh Machine Tools Factory (BMTF) aimed to be
+                        a central force in East Pakistan's industrial growth,
+                        focusing on supporting small enterprises in minor
+                        engineering products. Over time, it transformed into one
+                        of Bangladesh's major commercial enterprises, playing a
+                        crucial role in shaping the nation's industrial
+                        landscape since its inception.
+                      </p>
+                      <img src='http://placehold.it/1000x500' alt='Graphic' />
                     </div>
                   </div>
-                </section>
-              </div>
+
+                  <div class='demo-card demo-card--step2'>
+                    <div class='head'>
+                      <div class='number-box'>
+                        <span>02</span>
+                      </div>
+                      <h2>
+                        <span class='small'>In</span> 1967
+                      </h2>
+                    </div>
+                    <div class='body'>
+                      <p>
+                        The Bangladesh Machine Tools Factory (BMTF) officially
+                        launched its operation. Concurrently, construction
+                        efforts were underway, establishing the physical
+                        groundwork for BMTF's role as a significant player in
+                        Bangladesh's industrial development.
+                      </p>
+                      <img src='http://placehold.it/1000x500' alt='Graphic' />
+                    </div>
+                  </div>
+
+                  <div class='demo-card demo-card--step3'>
+                    <div class='head'>
+                      <div class='number-box'>
+                        <span>03</span>
+                      </div>
+                      <h2>
+                        <span class='small'>In</span> 1972
+                      </h2>
+                    </div>
+                    <div class='body'>
+                      <p>
+                        The operational reins of the Bangladesh Machine Tools
+                        Factory (BMTF) were handed over to the Bangladesh Steel
+                        & Engineering Corporation. This strategic decision aimed
+                        to optimize efficiency and enhance the factory's
+                        performance under the specialized management of a
+                        renowned entity.
+                      </p>
+                      <img src='http://placehold.it/1000x500' alt='Graphic' />
+                    </div>
+                  </div>
+
+                  <div class='demo-card demo-card--step4'>
+                    <div class='head'>
+                      <div class='number-box'>
+                        <span>04</span>
+                      </div>
+                      <h2>
+                        <span class='small'>In</span> 1979
+                      </h2>
+                    </div>
+                    <div class='body'>
+                      <p>
+                        Following the independence of Bangladesh, a remarkable
+                        journey started on February 11, 1979, with the official
+                        inauguration of the Bangladesh Machine Tools Factory
+                        (BMTF). This marked a crucial step in the nation's
+                        post-independence industrial development.
+                      </p>
+                      <img src='http://placehold.it/1000x500' alt='Graphic' />
+                    </div>
+                  </div>
+
+                  <div class='demo-card demo-card--step5'>
+                    <div class='head'>
+                      <div class='number-box'>
+                        <span>05</span>
+                      </div>
+                      <h2>
+                        <span class='small'>From</span> 1994-2000
+                      </h2>
+                    </div>
+                    <div class='body'>
+                      <p>The factory was closed during this tenure.</p>
+                      <img src='http://placehold.it/1000x500' alt='Graphic' />
+                    </div>
+                  </div>
+                  <div class='demo-card demo-card--step5'>
+                    <div class='head'>
+                      <div class='number-box'>
+                        <span>06</span>
+                      </div>
+                      <h2>
+                        <span class='small'>In</span> 2000
+                      </h2>
+                    </div>
+                    <div class='body'>
+                      <p>
+                        On July 27, 2000 Honorable Prime Minister Sheikh Hasina
+                        handed over The BMTF industry to Bangladesh Army to
+                        increase the efficiency. The whole operating system of
+                        BMTF is officially going under Bangladesh Army and it
+                        has been a remarkable journey since then.{" "}
+                      </p>
+                      <img src='http://placehold.it/1000x500' alt='Graphic' />
+                    </div>
+                  </div>
+                </div>
+              </section>
             </div>
             {/*History Timeline End */}
           </div>
